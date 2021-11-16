d8ba0ff110d161f6970365116e901d68944544c0
ollylomax <ollylomax@yahoo.com>
2021-11-16 11:28:50 +0000

    Fix bug with commas rendering at top of table

--- main.js
+++ main.js
@@ -62,7 +62,7 @@ function writeToDocument(url) {
 
         });
 
-        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>${pagination}`;
+        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>${pagination}`.replace(/,/g, "");
 
     });
 
